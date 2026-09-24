a53460a2f27a7aeb499cd54a2001144e9a846a1f
Rajkeser <rajkeshar.2024ug2073@iiitranchi.ac.in>
2026-09-24 12:17:09 +0530

    Updated food delivery features

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://rajkesharyadav91_db_user:leDBKALyx8PNjszV@cluster0.nsvx00i.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGODB_URI).then(()=>console.log("DB Connected"));
 }
